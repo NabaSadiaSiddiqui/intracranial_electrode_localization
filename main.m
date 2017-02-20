0022d7acfd749b204f5709abe23253ce1df6680e
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2017-02-19 19:25:45 -0500

    Allow adding more grids

--- main.m
+++ main.m
@@ -1,11 +1,31 @@
 
 h_figure_whole = figure('MenuBar', 'none', 'Color', 'white');
 
+global grids;
+grids = [];
+
+global h_grid;
 h_grid = Grid();
 h_grid.setup_electrode_grid();
 
+h_menu = uimenu(h_figure_whole,'Label','Grid');
+menu_grid_add = uimenu(h_menu, 'Label', 'Add Grid', 'callback', {@new_grid});
+
 NeuroimagingWindow.mock_2D_window();
 
-function hello
-    disp('Naba');
+
+
+function new_grid(src, event, handles)
+    global h_grid;
+    aGrid = struct('name', h_grid.GridName.String, 'xDim', h_grid.GridDimensionsX.String, 'yDim', h_grid.GridDimensionsY.String);
+    
+    global grids;
+    grids = [grids, aGrid];
+
+    h_grid = Grid();
+    h_grid.setup_electrode_grid();
+    
+    for grid = grids
+        disp(grid.name);
+    end
 end
