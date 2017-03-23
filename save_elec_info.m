30dd39e3e9d0377c8368ed0745629d96bc785c59
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2017-03-22 21:48:03 -0400

    DRY - refactor logic of adding new grid to the data structure to a separate function; Ensure that an item is added to the data structure ONLY if it exists

--- save_elec_info.m
+++ save_elec_info.m
@@ -1,27 +1,7 @@
 function save_elec_info(src, event, handles)
-    global h_grid;
-    
-    keys_marked_electrodes = keys(h_grid.GridMarkedElectrodes);
-    values_marked_electrodes = values(h_grid.GridMarkedElectrodes);
-    marked_electrodes_map_to_str = '';
-    for i = 1:length(h_grid.GridMarkedElectrodes)
-        marked_electrode = strcat('[', keys_marked_electrodes{i}, ',', values_marked_electrodes{i}, ']');
-        marked_electrodes_map_to_str = strcat(marked_electrodes_map_to_str, marked_electrode);
-    end
-    
-    disabled_electrodes = '';
-    if ~isempty(h_grid.GridDisabledElectrodes)
-        disabled_electrodes = h_grid.GridDisabledElectrodes;
-    end
-    
-    aGrid = struct('name', h_grid.GridName.String, ...
-        'xDim', h_grid.GridDimensionsX.String, ...
-        'yDim', h_grid.GridDimensionsY.String, ...
-        'markedElectrodes', marked_electrodes_map_to_str, ...
-        'disabledElectrodes', disabled_electrodes);
+    grid_add();
     
     global grids;
-    grids = [grids, aGrid];
     
     fid = fopen('result.txt', 'wt+');
     fprintf(fid, 'GridName,GridRows,GridCols,MarkedElectrodes,DisabledElectrodes\n');
