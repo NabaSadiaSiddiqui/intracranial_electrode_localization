d49647b4bbbf6f187750becead343b47621a817c
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2017-04-01 14:22:03 -0400

    Computing electrode centroid - Make slider increment by discrete values - Propagate selected value to rendering logic

--- MRIcroS/+electrode/GeometryController.m
+++ MRIcroS/+electrode/GeometryController.m
@@ -14,23 +14,20 @@ classdef GeometryController
         function redraw(this, hAxes)
             this.surf.redraw(hAxes);
         end
-        function centroid = marker_by_point(this, P)
+        function centroid = marker_by_point(this, P, radius)
             if ~isempty(this.vol)
-                centroid = this.marker_by_point_vol(P);
+                centroid = this.marker_by_point_vol(P, radius);
             else
                 centroid = this.surf.marker_by_point(P);
             end
         end
-        function set_volume_trace_radius(this, radius)
-            disp(radius);
-        end
     end
     methods(Access = protected)
-        function centroid = marker_by_point_vol(this, P)
+        function centroid = marker_by_point_vol(this, P, radius)
             % identify 3mm x 3mm x 3mm rhombohedron
             % affine -> linear (dispose of translation)
             lin = this.hdr.mat(1:3, 1:3);
-            L = round(3 ./ sqrt(sum(lin.^2, 1))).'; % pixels in respective direction to attain 3mm edge length
+            L = round(radius ./ sqrt(sum(lin.^2, 1))).'; % pixels in respective direction to attain 3mm edge length
             P_prime = round(this.hdr.mat \ [P.'; 1]); % remap click on surf to image coordinate
             lower = P_prime(1:3) - ceil(L./2);
             sample = this.vol(lower(1):lower(1)+L(1)-1, lower(2):lower(2)+L(2)-1, lower(3):lower(3)+L(3)-1);
